2933addee4eefd86bba7206c6e3b3eea8aefe563
jwstes <jwstes@gmail.com>
2018-02-21 10:27:32 +0800

    Add files via upload

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_vsc = new MetroFramework.Controls.MetroButton();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.metroComboBox1 = new MetroFramework.Controls.MetroComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.metroToggle1 = new MetroFramework.Controls.MetroToggle();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(23, 166);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(432, 363);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(97, 63);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(284, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "youtube.com";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_vsc
+            // 
+            this.btn_vsc.Location = new System.Drawing.Point(23, 98);
+            this.btn_vsc.Name = "btn_vsc";
+            this.btn_vsc.Size = new System.Drawing.Size(114, 29);
+            this.btn_vsc.Style = MetroFramework.MetroColorStyle.Purple;
+            this.btn_vsc.TabIndex = 3;
+            this.btn_vsc.Text = "View Source Code";
+            this.btn_vsc.Click += new System.EventHandler(this.btn_vsc_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(23, 63);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 29);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "http://";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // metroComboBox1
+            // 
+            this.metroComboBox1.FormattingEnabled = true;
+            this.metroComboBox1.ItemHeight = 23;
+            this.metroComboBox1.Items.AddRange(new object[] {
+            "Only view source code",
+            "Download file as index.html",
+            "Download file as main.html"});
+            this.metroComboBox1.Location = new System.Drawing.Point(143, 98);
+            this.metroComboBox1.Name = "metroComboBox1";
+            this.metroComboBox1.Size = new System.Drawing.Size(312, 29);
+            this.metroComboBox1.Style = MetroFramework.MetroColorStyle.Purple;
+            this.metroComboBox1.TabIndex = 5;
+            this.metroComboBox1.SelectedIndexChanged += new System.EventHandler(this.metroComboBox1_SelectedIndexChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(387, 63);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(68, 29);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "/";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // metroToggle1
+            // 
+            this.metroToggle1.AutoSize = true;
+            this.metroToggle1.Location = new System.Drawing.Point(158, 135);
+            this.metroToggle1.Name = "metroToggle1";
+            this.metroToggle1.Size = new System.Drawing.Size(80, 17);
+            this.metroToggle1.Style = MetroFramework.MetroColorStyle.Purple;
+            this.metroToggle1.TabIndex = 7;
+            this.metroToggle1.Text = "Off";
+            this.metroToggle1.UseVisualStyleBackColor = true;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 133);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(129, 19);
+            this.metroLabel1.TabIndex = 8;
+            this.metroLabel1.Text = "Save file to desktop?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(478, 585);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.metroToggle1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.metroComboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btn_vsc);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Style = MetroFramework.MetroColorStyle.Purple;
+            this.Text = "ViewHTML - jwstes";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private MetroFramework.Controls.MetroButton btn_vsc;
+        private System.Windows.Forms.TextBox textBox2;
+        private MetroFramework.Controls.MetroComboBox metroComboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private MetroFramework.Controls.MetroToggle metroToggle1;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using MetroFramework;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static WebClient client = new WebClient();
+
+        static string GetCode(string placeholder)
+        {
+           string code = client.DownloadString(placeholder);
+           client.DownloadFile(placeholder, "index.html");
+           placeholder = code;
+
+           return code;
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_vsc_Click(object sender, EventArgs e)
+        {
+            string placeholder;
+            if (metroComboBox1.SelectedIndex == 0)
+            {
+                richTextBox1.Text = "";
+
+                placeholder = textBox2.Text + textBox1.Text + textBox3.Text;
+
+                richTextBox1.Text = GetCode(placeholder);
+            }
+            else if (metroComboBox1.SelectedIndex == 1)
+            {
+                richTextBox1.Text = "";
+
+                placeholder = textBox2.Text + textBox1.Text + textBox3.Text;
+                if (metroToggle1.Checked == true)
+                {
+                    string path = @"C:\Users\" + Environment.UserName + @"\Desktop\index.html";
+                    client.DownloadFile(placeholder, path);
+                }
+                else
+                {
+                    client.DownloadFile(placeholder, "index.html");
+
+                }
+                richTextBox1.Text = GetCode(placeholder);
+            }
+            else if (metroComboBox1.SelectedIndex == 2)
+            {
+                richTextBox1.Text = "";
+
+                placeholder = textBox2.Text + textBox1.Text + textBox3.Text;
+                if (metroToggle1.Checked == true)
+                {
+                    string path = @"C:\Users\" + Environment.UserName + @"\Desktop\main.html";
+                    client.DownloadFile(placeholder, path);
+                }
+                else
+                {
+                    client.DownloadFile(placeholder, "main.html");
+
+                }
+                richTextBox1.Text = GetCode(placeholder);
+            }
+            else
+            {
+                richTextBox1.Text = "Select a method!";
+            }
+        }
+
+        private void metroComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (metroComboBox1.SelectedIndex == 0)
+            {
+                metroLabel1.Enabled = false;
+                metroToggle1.Enabled = false;
+            }
+            else if (metroComboBox1.SelectedIndex == 1)
+            {
+                metroLabel1.Enabled = true;
+                metroToggle1.Enabled = true;
+            }
+            else if (metroComboBox1.SelectedIndex == 2)
+            {
+                metroLabel1.Enabled = true;
+                metroToggle1.Enabled = true;
+            }
+            else
+            {
+                metroLabel1.Enabled = false;
+                metroToggle1.Enabled = false;
+            }
+        }
+
+    }
+}
